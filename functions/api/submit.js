ffd44981872db7d5155b6891194f5b08c5d9cc06
Daniel Sleeth <5717589+MrSleeth@users.noreply.github.com>
2026-01-16 14:42:45 +0000

    Update form

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -1,29 +1,63 @@
-/**
- * POST /api/submit
- */
-export async function onRequestPost(context) {
-  try {
-    let input = await context.request.formData();
-
-    // Convert FormData to JSON
-    // NOTE: Allows multiple values per key
-    let output = {};
-    for (let [key, value] of input) {
-      let tmp = output[key];
-      if (tmp === undefined) {
-        output[key] = value;
-      } else {
-        output[key] = [].concat(tmp, value);
-      }
-    }
-
-    let pretty = JSON.stringify(output, null, 2);
-    return new Response(pretty, {
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
+async function submitHandler(request, env) {
+  if (request.method !== "POST") {
+    return new Response("Method Not Allowed", {
+      status: 405,
     });
-  } catch (err) {
-    return new Response("Error parsing JSON content", { status: 400 });
+  }
+  const body = await request.formData();
+
+  const { name, coming, main_meal, dessert, non_drinker, more_info } =
+    Object.fromEntries(body);
+
+  // The keys in "fields" are case-sensitive, and
+  // should exactly match the field names you set up
+  // in your Airtable table, such as "First Name".
+  const reqBody = {
+    fields: {
+      Name: name,
+      RSVP: coming,
+      "Main Meal": main_meal,
+      Dessert: dessert,
+      Dietary: dessert,
+      Alcohol: non_drinker,
+      Other: more_info
+    },
+  };
+  await createAirtableRecord(env, reqBody);
+}
+
+async function createAirtableRecord(env, body) {
+  try {
+    const result = fetch(
+      `https://api.airtable.com/v0/${env.AIRTABLE_BASE_ID}/${encodeURIComponent(env.AIRTABLE_TABLE_NAME)}`,
+      {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+          Authorization: `Bearer ${env.AIRTABLE_ACCESS_TOKEN}`,
+          "Content-Type": "application/json",
+        },
+      },
+    );
+    return result;
+  } catch (error) {
+    console.error(error);
   }
 }
+
+// Existing code
+// async function submitHandler
+// export default ...
+
+// Existing code
+// export default ...
+
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    if (url.pathname === "/api/submit") {
+      await submitHandler(request, env);
+    }
+    return new Response("Not found", { status: 404 });
+  },
+};
