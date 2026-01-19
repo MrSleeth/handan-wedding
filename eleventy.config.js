b1b00f16624ce22885ab4129d60ab0d84a8ad7da
Daniel Sleeth <5717589+MrSleeth@users.noreply.github.com>
2026-01-19 14:56:29 +0000

    Update config

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,13 +1,9 @@
 export default async function (eleventyConfig) {
 	// Reset to this value
-	eleventyConfig.setTemplateFormats("njk, html");
+	eleventyConfig.setTemplateFormats("njk, liquid, html");
 
 	eleventyConfig.addPassthroughCopy("_worker.js");
 	eleventyConfig.addPassthroughCopy("bundle.css");
 	eleventyConfig.addPassthroughCopy("assets/fonts");
 	eleventyConfig.addPassthroughCopy("assets/images");
-};
-
-export const config = {
-  markdownTemplateEngine: "njk",
 };
